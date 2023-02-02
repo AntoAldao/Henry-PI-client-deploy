56ac14ee89a21990075e6a40d09e3913abee9d34
AntoAldao <antoaldao@gmail.com>
2023-02-01 21:24:09 -0300

    not found

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route,Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Game from './pages/Game/Game';
 import Create from './pages/Create/Create';
 import axios from 'axios';
+import notFound from './pages/NotFound/NotFound';
 axios.defaults.baseURL = 'https://henry-pi-production.up.railway.app/api/';
 
 
@@ -12,10 +13,13 @@ axios.defaults.baseURL = 'https://henry-pi-production.up.railway.app/api/';
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={LandingPage} />
-      <Route exact path="/home" component={Home} />
-      <Route exact path="/videogame/:id" component={Game} />
-      <Route exact path="/create" component={Create} />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/videogame/:id" component={Game} />
+        <Route exact path="/create" component={Create} />
+        <Route component={notFound} />
+      </Switch>
 
 
     </div>
---
+++ client/src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import style from "./NotFound.module.css";
+const notFound = () => {
+    return (
+        <div className={style.gdiv}>
+            <h1 className={style.notFound}>404 Not Found</h1>
+            
+        </div>
+    );
+};
+export default notFound;
